758d92ae34e13ebec7914cd28af9c5ea9abd99d9
Corbin Crutchley <git@crutchcorn.dev>
2024-12-11 15:18:23 -0800

    test: add fetch page icon test

--- worker/src/url-metadata/fetch-page-icon.spec.ts
+++ worker/src/url-metadata/fetch-page-icon.spec.ts
@@ -9,8 +9,28 @@ import {
 import { removePositions } from "../../test-utils/hast";
 import { fetchPageIcon } from "./fetch-page-icon";
 
-test("Should fetch basic page icon", () => {
+test("Should fetch basic page icon", async () => {
+	const html = `
+<!DOCTYPE html>
+<head>
+	<title>Test</title>
+	<link rel="shortcut icon" type="image/png" href="/img.png">
+</head>
+<body>
+	<h1>Test</h1>
+</body>
+</html>
+`.trim();
+
 	const domain = "https://example.com/test";
-	mockEndpoint({ path: domain, body: "", method: "get", status: 400 });
-	fetchPageIcon;
+	mockEndpoint({
+		path: domain,
+		body: html,
+		method: "get",
+		bodyType: "text",
+	});
+	const srcHast = await fetchPageHtml(new URL(domain));
+	const iconHref = await fetchPageIcon(new URL(domain), srcHast!);
+
+	expect(iconHref).toEqual(new URL("https://example.com/img.png"));
 });
